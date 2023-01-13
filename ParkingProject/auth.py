314d7cc75d5b951d58af891c89a7d0ad7bbc7d56
titintoro <titintola@gmail.com>
2023-01-13 17:52:33 +0100

    Changes

--- ParkingProject/auth.py
+++ ParkingProject/auth.py
@@ -3,3 +3,4 @@ def authentication ():
     res = input("\nIndique su rol en el parking (PRESIONE 1 o 2):"
                 "\n\t1. Cliente"
                 "\n\t2. Administrador")
+    return res
--- ParkingProject/main.py
+++ ParkingProject/main.py
@@ -1,4 +1,13 @@
 from auth import authentication
 
 print("<< PARKING BOSCO >>")
-authentication()
+
+authType = int(authentication())
+
+print(authType)
+
+if authType==1:
+    print("Ha entrado como cliente")
+
+if authType==2:
+    print("Ha entrado como administrador")
